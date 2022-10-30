7c659f7199e6858cfe1b3fd626234a1813fe335f
vandu1979 <vandana_kothari@yahoo.com>
2022-10-30 11:51:48 -0600

    Hello

---
+++ index.js
@@ -0,0 +1,46 @@
+// let name = "vandu";
+// console.log(name);
+// let firstName = "Vandana";
+// let lastName = "Kothari";
+// console.log(firstName,lastName);
+// const interestRate = 0.3;
+// interestRate = 5;
+//console.log(interestRate);
+// object
+
+//console.log(person);
+//Dot Notation to change person properties
+//person.name = "John";
+// Bracket Notation
+// let selection = "name";
+// person[selection] = "Mary";
+// person["name"] = "Mary";
+// console.log(person.name);
+// Arrays
+// let selectedColors = ["red", "blue"];
+// selectedColors[2] = "green";
+// selectedColors[2] = 1;
+// console.log(selectedColors);
+//console.log(selectedColors.length);
+
+//Function
+// function greet() {
+//  console.log('Hello World');   
+// }
+
+// greet();
+//Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('John', 'Smith');
+greet('Mary', 'Com');
+
+// calculating a value
+function square(number) {
+   return number * number;   
+}
+//let number = square(2)  (not necessarily declear a varible )
+console.log(square(2));
+//Object Oriented Programming
